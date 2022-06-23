cd5f369bf7938c6c5c95d39f5dccf304b80190a4
mayuryes99 <mayuryes99@gmail.com>
2022-06-24 03:02:33 +0530

    pull

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-res.send('Hello Everyone Task is for Opstree Solution.');
+res.send('Hello Everyone Task is for Opstree Solution, Docker-Push');
 }); 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
